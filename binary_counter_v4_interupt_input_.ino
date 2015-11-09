ebd880104fb43801210d82387cbb796423f3330c
Jonathon Pinnow <pinnowj@gmail.com>
2015-11-09 17:02:27 -0500

    changed debounce time

--- binary_counter_v4_interupt_input_.ino
+++ binary_counter_v4_interupt_input_.ino
@@ -60,7 +60,7 @@ void countUp() {
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
 
-  if (interrupt_time - last_interrupt_time > 100) {
+  if (interrupt_time - last_interrupt_time > 150) {
     if (digitalRead(buttonPins[1]) == LOW) {
       total_1 --;
     }
